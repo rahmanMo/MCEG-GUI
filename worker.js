d3c3dfd364ab4977496afa183bb0585ef10cb782
adnanmc <adnanmcj@gmail.com>
2017-11-20 17:40:26 -0500

    failed conversion to json file: data format incorrect

--- worker.js
+++ worker.js
@@ -1,14 +1,28 @@
 const pythonShell = require('python-shell');
 const fs = require('fs-extra');
+const csvFilePath='./data/data.csv';
+const csvToJSON=require('csvtojson');
 
 setTimeout(function () {
-  // With Promises
-  fs.writeJson('./data/data.json', {name: 'fs-extra'})
-  .then(() => {
-    console.log('success!')
+
+
+  csvToJSON()
+  .fromFile(csvFilePath)
+  .on('json',(jsonObj)=>{
+    // combine csv header row and csv line to a json object
+    // jsonObj.a ==> 1 or 4
+    // With Promises
+      fs.writeJson('./data/data.json', jsonObj )
+      .then(() => {
+        console.log('success!')
+      })
+      .catch(err => {
+        console.error(err)
+      });
   })
-  .catch(err => {
-    console.error(err)
-  })
+  .on('done',(error)=>{
+    console.log('end')
+  });
+
 },15000);
 
