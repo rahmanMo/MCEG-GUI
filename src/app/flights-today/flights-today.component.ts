304be604fb77c12d98b70154c6384096b23a585d
adnanmc <adnanmcj@gmail.com>
2017-11-29 18:07:51 -0500

    now component does live refresh without recreating dom

--- src/app/flights-today/flights-today.component.ts
+++ src/app/flights-today/flights-today.component.ts
@@ -13,25 +13,73 @@ export class FlightsTodayComponent implements OnInit {
 
   constructor(private flightsService: FlightsService) { }
   todaysFlights: Flight[];
+  // todaysFlights: Flight[] = [{"recordStatus": "",
+  //                  "lastDateModified": "25NOV17",
+  //                   "lastTimeModified": "1213",
+  //    "lastUserToModify": "PLNK",
+  //     "legDepartureDate": "28NOV17",
+  //      "airlineCode": "JBU",
+  //       "identifier": "   1 ",
+  //        "sequence": "10", "flightOriginDay": "28",
+  //         "numericFlightDate": "20171128",
+  //          "numGMTDate": "20171128",
+  //           "STDudt": "1549",
+  //            "STAudt": "1849",
+  //             "tailNumber": "952   ",
+  //              "numLastDateModified": "20171125",
+  //               "flightStatus": "SC05",
+  //                "origin": "JFK ",
+  //                 "STDLocal": "1049",
+  //                  "dispatchDesk": "DM",
+  //                   "STDGMTVariance": "-0500",
+  //                    "destination": "FLL ",
+  //                     "STALocal": "1349",
+  //                      "STAGMTVariance": "-0500",
+  //                       "OAGEquipmentType": "321 ",
+  //                        "ACConfiguration": "",
+  //                         "serviceType": "",
+  //                          "originGate": "",
+  //                           "ETDlocal": "1049",
+  //                            "ETDudt": "1549",
+  //                             "TAXIutc": "",
+  //                              "OUTudt": "2055",
+  //                               "OFFudt": "",
+  //                                "destinationGate": "",
+  //                                 "ETAlocal": "1349",
+  //                                  "ETAudt": "1849",
+  //                                   "ONudt": "",
+  //                                    "INudt": "",
+  //                                     "previousTailNumber": "971 ",
+  //                                      "ETE": "",
+  //                                       "DCNutc": "",
+  //                                        "ETOutc": "",
+  //                                         "EONutc": "",
+  //                                          "EDTCutc": "",
+  //                                           "flightType": "0",
+  //                                            "newDepartureCity": "",
+  //                                             "newArrivalCity": "",
+  //                                              "SchedOAGEquipType": "321 ",
+  //                                               "OAGEquipSubType": "",
+  //                                                "csvFSDailyID": "4519000",
+  //                                                 "tailNumBeforeCancel": "",
+  //                                                  "CTAUTC": "",
+  //                                                   "cancelled": "",
+  //                                                    "replaced": "0",
+  //                                                     "ATCStatus": "0",
+  //                                                      "scheduledFlightType": "0",
+  //                                                       "aircraftRouting": "> 202 ",
+  //                                                        "mealService": "", "hub": "JFK",
+  //                                                         "landingRestriction": "3",
+  //                                                          "headStartFlight": "0",
+  //                                                           "actualDeparture": "2017-11-28 15:49:00.000",
+  //                                                            "specialFlight": "0",
+  //                                                             "actualArrival": "2017-11-28 18:49:00.000",
+  //                                                              "scheduledTaxiOut": "29",
+  //                                                               "scheduledTaxiIn": "7",
+  //                                                                "STOSetByUser": "0",
+  //                                                                 "STISetByUser": "0",
+  //                                                                  "CTFlightNumber": "0001"}];
 
-
-  // ngOnInit() {
-  //   const sec = 10;
-  //   Observable.timer(0, sec * 1000).subscribe(this.getWhatsUp());
-  // }
-
-  // getTodaysFlights() {
-  //   this.flightsService.getTodaysFlights()
-  //     .subscribe(flights => this.todaysFlights = flights,
-  //                 err => console.log(err));
-  // }
-
-
-  // ngOnInit() {
-  //   this.flightsService.getTodaysFlights()
-  //   .subscribe(flights => this.todaysFlights = flights,
-  //               err => console.log(err));
-  // }
   interval: any;
 
   ngOnInit() {
@@ -47,5 +95,7 @@ export class FlightsTodayComponent implements OnInit {
               this.todaysFlights = data;
           });
     }
+
+  trackByFlights(index: number, flight: Flight): number { return index; }
 
   }
--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Flight } from '../models/flight';
 import { Observable } from 'rxjs/Observable';
+// import { MessageService } from './message.service';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -10,17 +11,25 @@ export class FlightsService {
 
   constructor(private http: HttpClient) { }
 
+  // private flightsUrl = 'http://localhost:3000/api/flights';
   private flightsUrl = '/api/flights';
 
-  private handleError(error: any) {
-    if (error instanceof Response) {
-      return Observable.throw(error.json()['error'] || 'backend server error');
-    }
-    return Observable.throw(error || 'backend server error');
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   getTodaysFlights(): Observable<Flight[]> {
-    return this.http.get<Flight[]>(this.flightsUrl);
+    return this.http.get<Flight[]>(this.flightsUrl)
+      .pipe(
+        catchError(this.handleError('getTodaysFlights', []))
+      );
   }
 
   // getTodaysFlights(): Observable<Flight[]> {
