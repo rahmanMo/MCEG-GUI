d12ce8e0ff7323327bc60847c30b94ed1edd691e
adnanmc <adnanmcj@gmail.com>
2018-05-24 13:54:30 -0400

    WIP publish event from api: ON IN added

--- server/routes/event-api.js
+++ server/routes/event-api.js
@@ -9,7 +9,7 @@ const v = require('voca');
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////// each events ////////////////////////////////////////
 
-//////////////////////// departure gate change (OUT) ////////////////////////////
+//////////////////////// OUT ////////////////////////////
 /*
 Required params:
 {
@@ -58,7 +58,7 @@ router.post('/out', async (req, res) => {
       } else if (v(flightData[0].cancelled).trim() == 'X') {
         res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} is cancelled.`});
       } else if (v(flightData[0].previousTailNumber).trim() == 'CANX' || v(flightData[0].tailNumber).startsWith('-', 0)) {
-        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} had airturnback or ground turnback or divert-continue etc. You need to login to MVT to change this flight.`});
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} had air turnback or ground turnback or divert-continue etc. You need to login to MVT to change this flight.`});
       } else if (!v(flightData[0].OFFudt).trim() == '' || !v(flightData[0].ONudt).trim() == '' || !v(flightData[0].INudt).trim() == '') {
         res.status(404).json({ error : `flight ${flightNum} for day ${day} already has OFF or ON or IN. Please use RMA (remove arrival) or RMD (remove departure) before setting new OUT`});
       } else {
@@ -68,9 +68,8 @@ router.post('/out', async (req, res) => {
         let date = v.trim(flightData[0].numGMTDate);
         let origin = v.trim(flightData[0].origin);
         let dest = v.trim(flightData[0].destination);
-        let std = v.trim(flightData[0].STDudt);
+        let std = v(flightData[0].STDudt).trim().padLeft(4, '0');
         let out = v(outUTC).padLeft(4, '0');
-        let now = moment(new Date()).format('MM_DD_YYYY_HH_mm_SS_x');
         let dropLocation;
         if (stg == 'stg1') {
           dropLocation = './sample';
@@ -79,6 +78,7 @@ router.post('/out', async (req, res) => {
         } else if (stg == 'stg3') {
           dropLocation = './sample';
         }
+        let now = moment(new Date()).format('MM_DD_YYYY_HH_mm_SS_x');
         let fileName = `mceg_adhoc16_out_${now}`;
         let adhocString = `ADH016_${pFlightNum}${date}${origin}${dest}${std}OUT${out}`;
         let job = await fs.writeFile(`${dropLocation}/${fileName}.txt`, adhocString).then((err) => {
@@ -100,7 +100,7 @@ router.post('/out', async (req, res) => {
 
 });
 
-//////////////////////// departure gate change (OFF) ////////////////////////////
+////////////////////////  OFF  ////////////////////////////
 /*
 Required params:
 {
@@ -149,7 +149,7 @@ router.post('/off', async (req, res) => {
       } else if (v(flightData[0].cancelled).trim() == 'X') {
         res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} is cancelled.`});
       } else if (v(flightData[0].previousTailNumber).trim() == 'CANX' || v(flightData[0].tailNumber).startsWith('-', 0)) {
-        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} had airturnback or ground turnback or divert-continue etc. You need to login to MVT to change this flight.`});
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} had air turnback or ground turnback or divert-continue etc. You need to login to MVT to change this flight.`});
       } else if (v(flightData[0].OUTudt).trim() == '') {
         res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} has no OUT time. Please send OUT event first.`});
       } else if (!v(flightData[0].ONudt).trim() == '' || !v(flightData[0].INudt).trim() == '') {
@@ -161,9 +161,9 @@ router.post('/off', async (req, res) => {
         let date = v.trim(flightData[0].numGMTDate);
         let origin = v.trim(flightData[0].origin);
         let dest = v.trim(flightData[0].destination);
-        let std = v.trim(flightData[0].STDudt);
+        let std = v(flightData[0].STDudt).trim().padLeft(4, '0');
+        let out = v(flightData[0].OUTudt).trim().padLeft(4, '0');
         let off = v(offUTC).padLeft(4, '0');
-        let now = moment(new Date()).format('MM_DD_YYYY_HH_mm_SS_x');
         let dropLocation;
         if (stg == 'stg1') {
           dropLocation = './sample';
@@ -172,8 +172,9 @@ router.post('/off', async (req, res) => {
         } else if (stg == 'stg3') {
           dropLocation = './sample';
         }
+        let now = moment(new Date()).format('MM_DD_YYYY_HH_mm_SS_x');
         let fileName = `mceg_adhoc16_off_${now}`;
-        let adhocString = `ADH016_${pFlightNum}${date}${origin}${dest}${std}OFF${off}`;
+        let adhocString = `ADH016_${pFlightNum}${date}${origin}${dest}${std}OFF${out}${off}`;
         let job = await fs.writeFile(`${dropLocation}/${fileName}.txt`, adhocString).then((err) => {
          if (err) {
           console.log(err)
@@ -194,6 +195,188 @@ router.post('/off', async (req, res) => {
 });
 
 
+//////////////////////// ON ////////////////////////////
+/*
+Required params:
+{
+  "stg":"stg1",
+  "day":"d0", ( up to d7 available, d0 is yesterday, d1 is today and so on)
+  "flightNum":"55",
+  "onUTC":"1245"
+}
 
+conditions: Flight must have OUT and OFF value. If flight  already has IN value tell user to to use RMA (remove arrival)
+
+
+*/
+router.post('/on', async (req, res) => {
+  let body = req.body;
+  let stg = v.trim(body.stg);
+  let day = v.trim(body.day);
+  let flightNum = v.trim(body.flightNum);
+  let onUTC = v.trim(body.onUTC);
+  if (!stg == 'stg1' || !stg == 'stg2' || !stg == 'stg3') {
+    res.status(400).json({ error: 'stg must be stg1 or stg3' });
+  } else if (
+    !day == 'd0' ||
+    !day == 'd1' ||
+    !day == 'd2' ||
+    !day == 'd3' ||
+    !day == 'd4' ||
+    !day == 'd5' ||
+    !day == 'd6' ||
+    !day == 'd7'
+  ) {
+    res.status(400).json({ error: 'day must be d#; # is in range 0 to 7; ex: d0 is yesterday d1 is today.' });
+  } else if (!v.count(flightNum) >= 1 && !v.count(flightNum) <= 4 && isNaN(flightNum)) {
+    res.status(400).json({ error: 'flightNum must be number minimum 1 and maximum 4 digit.' });
+  } else if (!v.count(onUTC) >= 1 && !v.count(onUTC) <= 4 && isNaN(onUTC)) {
+    res.status(400).json({ error: 'onUTC must be number minimum 1 and maximum 4 digit.' });
+  } else {
+    try {
+      // filter data by flight number
+      let flightData = await fetch(`http://localhost/api/${stg}/${day}`).then(res => res.json()).then(allData => allData.filter(x => v.trim(x.identifier) == flightNum && v.trim(x.sequence) == 10));
+
+
+      // handle no flight found + other exceptions
+      if (flightData == '' || flightData == {}) {
+        res.status(404).json({ error : `flight ${flightNum} not found for day ${day}`});
+      } else if (v(flightData[0].cancelled).trim() == 'X') {
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} is cancelled.`});
+      } else if (v(flightData[0].previousTailNumber).trim() == 'CANX' || v(flightData[0].tailNumber).startsWith('-', 0)) {
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} had air turnback or ground turnback or divert-continue etc. You need to login to MVT to change this flight.`});
+      } else if (v(flightData[0].OUTudt).trim() == '' || v(flightData[0].OFFudt).trim() == '' || !v(flightData[0].INudt).trim() == '') {
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} needs to have both OUT and OFF first. IN must be empty. Please use RMA (remove arrival) to remove IN before sending ON value`});
+      } else {
+
+        //////////////////////////////// prep data for adhoc 16 /////////////////////////////////
+        let pFlightNum = v.padLeft(flightNum, 4, '0');
+        let date = v.trim(flightData[0].numGMTDate);
+        let origin = v.trim(flightData[0].origin);
+        let dest = v.trim(flightData[0].destination);
+        let std = v(flightData[0].STDudt).trim().padLeft(4, '0');
+        let on = v(onUTC).padLeft(4, '0');
+        let dropLocation;
+        if (stg == 'stg1') {
+          dropLocation = './sample';
+        } else if (stg == 'stg2') {
+          dropLocation = './sample';
+        } else if (stg == 'stg3') {
+          dropLocation = './sample';
+        }
+        let now = moment(new Date()).format('MM_DD_YYYY_HH_mm_SS_x');
+        let fileName = `mceg_adhoc16_on_${now}`;
+        let adhocString = `ADH016_${pFlightNum}${date}${origin}${dest}${std}ON_${on}`;
+        let job = await fs.writeFile(`${dropLocation}/${fileName}.txt`, adhocString).then((err) => {
+         if (err) {
+          console.log(err)
+           res.status(404).json({error: `Error sending File: ${fileName} - ON for flight ${pFlightNum} departing utc ${date} Failed!!`});
+         } else {
+          res.status(201).json({adhoc: `File: ${fileName} sent at ${now} - ON sent for flight ${pFlightNum} departing utc ${date} with new ON: ${on}`});
+         }
+        });
+        ////////////////////////////////////// end of adhoc 16 /////////////////////////////////
+
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+});
+
+
+////////////////////////  IN  ////////////////////////////
+/*
+Required params:
+{
+  "stg":"stg1",
+  "day":"d0", ( up to d7 available, d0 is yesterday, d1 is today and so on)
+  "flightNum":"55",
+  "inUTC":"1245"
+}
+
+conditions: Flight must have OUT OFF ON already.
+
+
+*/
+router.post('/in', async (req, res) => {
+  let body = req.body;
+  let stg = v.trim(body.stg);
+  let day = v.trim(body.day);
+  let flightNum = v.trim(body.flightNum);
+  let inUTC = v.trim(body.inUTC);
+  if (!stg == 'stg1' || !stg == 'stg2' || !stg == 'stg3') {
+    res.status(400).json({ error: 'stg must be stg1 or stg3' });
+  } else if (
+    !day == 'd0' ||
+    !day == 'd1' ||
+    !day == 'd2' ||
+    !day == 'd3' ||
+    !day == 'd4' ||
+    !day == 'd5' ||
+    !day == 'd6' ||
+    !day == 'd7'
+  ) {
+    res.status(400).json({ error: 'day must be d#; # is in range 0 to 7; ex: d0 is yesterday d1 is today.' });
+  } else if (!v.count(flightNum) >= 1 && !v.count(flightNum) <= 4 && isNaN(flightNum)) {
+    res.status(400).json({ error: 'flightNum must be number minimum 1 and maximum 4 digit.' });
+  } else if (!v.count(inUTC) >= 1 && !v.count(inUTC) <= 4 && isNaN(inUTC)) {
+    res.status(400).json({ error: 'inUTC must be number minimum 1 and maximum 4 digit.' });
+  } else {
+    try {
+      // filter data by flight number
+      let flightData = await fetch(`http://localhost/api/${stg}/${day}`).then(res => res.json()).then(allData => allData.filter(x => v.trim(x.identifier) == flightNum && v.trim(x.sequence) == 10));
+
+
+      // handle no flight found + other exceptions
+      if (flightData == '' || flightData == {}) {
+        res.status(404).json({ error : `flight ${flightNum} not found for day ${day}`});
+      } else if (v(flightData[0].cancelled).trim() == 'X') {
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} is cancelled.`});
+      } else if (v(flightData[0].previousTailNumber).trim() == 'CANX' || v(flightData[0].tailNumber).startsWith('-', 0)) {
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} with local date ${flightData[0].numericFlightDate} had air turnback or ground turnback or divert-continue etc. You need to login to MVT to change this flight.`});
+      } else if (v(flightData[0].OUTudt).trim() == '' || v(flightData[0].OFFudt).trim() == '' || v(flightData[0].ONudt).trim() == '') {
+        res.status(404).json({ error : `flight ${flightNum} for day ${day} must have OUT OFF ON filled first before sending IN value`});
+      } else {
+
+        //////////////////////////////// prep data for adhoc 16 /////////////////////////////////
+        let pFlightNum = v.padLeft(flightNum, 4, '0');
+        let date = v.trim(flightData[0].numGMTDate);
+        let origin = v.trim(flightData[0].origin);
+        let dest = v.trim(flightData[0].destination);
+        let std = v(flightData[0].STDudt).trim().padLeft(4, '0');
+        let on = v(flightData[0].ONudt).trim().padLeft(4, '0');
+        let inValue = v(inUTC).padLeft(4, '0');
+        let dropLocation;
+        if (stg == 'stg1') {
+          dropLocation = './sample';
+        } else if (stg == 'stg2') {
+          dropLocation = './sample';
+        } else if (stg == 'stg3') {
+          dropLocation = './sample';
+        }
+        let now = moment(new Date()).format('MM_DD_YYYY_HH_mm_SS_x');
+        let fileName = `mceg_adhoc16_in_${now}`;
+        let adhocString = `ADH016_${pFlightNum}${date}${origin}${dest}${std}IN_${on}${inValue}`;
+        let job = await fs.writeFile(`${dropLocation}/${fileName}.txt`, adhocString).then((err) => {
+         if (err) {
+          console.log(err)
+           res.status(404).json({error: `Error sending File: ${fileName} - IN for flight ${pFlightNum} departing utc ${date} Failed!!`});
+         } else {
+          res.status(201).json({adhoc: `File: ${fileName} sent at ${now} - IN sent for flight ${pFlightNum} departing utc ${date} with new IN: ${inValue}`});
+         }
+        });
+        ////////////////////////////////////// end of adhoc 16 /////////////////////////////////
+
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+});
 
 module.exports = router;
