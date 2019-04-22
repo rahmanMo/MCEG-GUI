ee0bb2e30fe63ba8605c74624218038f47853b7c
adnanmc <adnanmcj@gmail.com>
2019-04-22 15:29:50 -0400

    seperated codebase for frontend and backend

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 // Set our api routes
 app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
--- src/app/models/adhoc-event.ts
+++
@@ -1,4 +0,0 @@
-export interface AdhocEvent {
-  stg: string;
-  adhoc16: string;
-}
